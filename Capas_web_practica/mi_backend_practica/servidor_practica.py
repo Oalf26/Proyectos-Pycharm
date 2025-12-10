ce097c3d49166b465e2f4a0d4ad20c1a85dd93de
Oalf26 <O.alfonsogf2011@gmail.com>
2025-12-10 11:27:46 -0500

    Mi_backend_prueba

--- Capas_web_practica/mi_backend_practica/servidor_practica.py
+++ Capas_web_practica/mi_backend_practica/servidor_practica.py
@@ -8,6 +8,11 @@ def Data():
         with open("database_practica.json", "r", encoding="utf-8") as f:
             data = json.load(f)
             return data
+def delete(item):
+    data = Data()
+    data.pop(item)
+    with open("database_practica.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
 def add(body):
     data = Data()
     data.append(body)
@@ -21,6 +26,20 @@ def query_string(url):
 
 
 class Myhandler(SimpleHTTPRequestHandler):
+    def http_headers(self):
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+    def write(self, message):
+        self.wfile.write(message)
+    def http_header_404(self):
+        self.send_response(404)
+        self.end_headers()
+    def add_information(self, information):
+        body = self.rfile.read(information)
+        data = body.decode("utf-8")
+        data = json.loads(data)
+        return data
+
     def do_GET(self):
         brand = query_string(self.path)
         path = par.urlparse(self.path)
@@ -31,13 +50,12 @@ class Myhandler(SimpleHTTPRequestHandler):
             except (json.JSONDecodeError, FileNotFoundError) as e:
                 self.send_response(500)
                 data = {"error": "Problema con la base de datos: " + str(e)}
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps(data).encode('utf-8'))
+            self.http_headers()
+            self.write(json.dumps(data).encode('utf-8'))
         elif self.path == '/ping':
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(b"Pong")
+            self.write(b"Pong")
         elif self.path == '/Carros' + "?" + path.query:
             try:
                 data = Data()
@@ -45,54 +63,73 @@ class Myhandler(SimpleHTTPRequestHandler):
             except (json.JSONDecodeError, FileNotFoundError) as e:
                 self.send_response(500)
                 data = {"error": "Problema con la base de datos: " + str(e)}
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
+            self.http_headers()
             for item in data:
                 if brand == item["brand"]:
-                    self.wfile.write(json.dumps(item["models"]).encode('utf-8'))
-                else:
-                    self.wfile.write(b"Modelo no existente")
+                    self.write(json.dumps(item["models"]).encode('utf-8'))
                     break
+                else:
+                    self.write(b"Modelo no existente")
+
         else:
-            self.send_response(404)
-            self.end_headers()
-            self.wfile.write(b"Ruta no encontrada")
+            self.http_header_404()
+            self.write(b"Ruta no encontrada")
     def do_POST(self):
         if self.path == "/Carros":
             try:
                 longitud = int(self.headers["Content-Length"])
-                body = self.rfile.read(longitud)
-                data = body.decode("utf-8")
-                data = json.loads(data)
+                data = self.add_information(longitud)
                 add(data)
                 self.send_response(201)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps({"Mensaje": "Informacion recibida"}).encode("utf-8"))
+                self.http_headers()
+                self.write(json.dumps({"Mensaje": "Informacion recibida"}).encode("utf-8"))
             except json.JSONDecodeError:
                 self.send_response(400)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps({"error": "JSON invalido"}).encode("utf-8"))
+                self.http_headers()
+                self.write(json.dumps({"error": "JSON invalido"}).encode("utf-8"))
 
             except Exception as e:
                 self.send_response(500)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps({"error": "Problema con la base de datos: " + str(e)}).encode("utf-8"))
+                self.http_headers()
+                self.write(json.dumps({"error": "Problema con la base de datos: " + str(e)}).encode("utf-8"))
+        else:
+            self.http_header_404()
+            self.write(b"Ruta no encontrada")
+    def do_DELETE(self):
+        brand = query_string(self.path)
+        path = par.urlparse(self.path)
+        if self.path == '/Carrosdelete' + "/brand" + "?" + path.query:
+                try:
+                    data = Data()
+                    self.send_response(201)
+                except json.JSONDecodeError:
+                    self.send_response(400)
+                    self.send_header("Content-type", "application/json")
+                    self.end_headers()
+                    self.wfile.write(json.dumps({"error": "JSON invalido"}).encode("utf-8"))
+                except Exception as e:
+                    self.send_response(500)
+                    self.send_header("Content-type", "application/json")
+                    self.end_headers()
+                    self.wfile.write(json.dumps({"error": "Problema con la base de datos: " + str(e)}).encode("utf-8"))
+                self.http_headers()
+                for item in data:
+                    if brand == item["brand"]:
+                        delete(item)
+                        self.write(json.dumps({"Mensaje": "Se ha eliminado con exito la marca:" + brand}).encode("utf-8"))
+                        break
+                    else:
+                        self.write(b"Modelo no existente")
+
         else:
             self.send_response(404)
             self.end_headers()
-            self.wfile.write(b"Ruta no encontrada")
+            self.write(b"Ruta no encontrada")
 PORT = 8000
 with socketserver.TCPServer(("", PORT), Myhandler) as httpd:
     print("Escuchando en el puerto" , PORT)
     httpd.serve_forever()
-#postman
-#Como mandar desde la url que tengo aqui un parametro por el metodo get hacia el backend
-#http://localhost:8080/carros?brand=Toyota
-#Modificar la función handle para que por el metodo get pueda recibir el parametro marca = brand
-#Este metodo debe ser por GET, el cual va a buscar la marca en base de datos y va a retornar el modelo de esa marca que le estas enviando.
-#Estudiar los metodos de comunicación http para comunicarse con servios REST
-#investigar como leer el json para mostrar el modelo de la marca que estas buscando.
-#ver un tutorial de como usar postman: como probar un get y post
+
+#Aprender significados de los codigos http
+#Identificar codigo reperido en mi codigo actual
+#Crear funciones para evitar repetir codigo e identificar variables
